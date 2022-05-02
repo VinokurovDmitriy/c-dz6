cf3be0c83828cd973f3a0b3956864b2dbe6c9ec5
dvvinokurov <dmitvitvin@gmail.com>
2022-05-02 15:52:43 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,11 @@
+﻿//Пользователь вводит с клавиатуры N чисел. Подсчитать количество положительных чисел
+Console.WriteLine("Введите количество чисел, которое хотите ввести");
+int n = Convert.ToInt32(Console.ReadLine());
+int countPositive = 0;
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Введите {i + 1} число из {n}");
+    if(Convert.ToInt32(Console.ReadLine()) > 0) countPositive++;
+}
+Console.WriteLine($"Вы ввели {countPositive} чисел больше нуля");
+
