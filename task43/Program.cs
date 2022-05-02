2b5a06ce2cf570c95b36fed916fa93a4d5410970
dvvinokurov <dmitvitvin@gmail.com>
2022-05-02 17:45:59 +0300

    task43

---
+++ task43/Program.cs
@@ -0,0 +1,21 @@
+﻿//найти точку пересечения прямых заданных уравнениями y = k1 * x + b1 и y = k2 * x + b2
+int[,] variables = new int[2, 2];
+string variableName;
+for (int i = 0; i < variables.GetLength(0); i++)
+{
+    for (int j = 0; j < variables.GetLength(1); j++)
+    {
+        if (j == 0) variableName = "b";
+        else variableName = "k";
+        Console.Write($"Введите зачение {variableName}{i + 1} = ");
+        variables[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int b1 = variables[0, 0];
+int k1 = variables[0, 1];
+int b2 = variables[1, 0];
+int k2 = variables[1, 1];
+Console.WriteLine($"vvvv = {variables[1, 0] * variables[0, 1] - variables[0, 0] * variables[1, 1]}  nnnn {variables[0, 1] - variables[1, 1]}");
+double x = (double) (variables[1, 0] - variables[0, 0]) / (variables[0, 1] - variables[1, 1]);
+double y = (double) (variables[1, 0] * variables[0, 1] - variables[0, 0] * variables[1, 1]) / (variables[0, 1] - variables[1, 1]);
+Console.WriteLine($"Точка пересечения прямых ({x}, {y})");
